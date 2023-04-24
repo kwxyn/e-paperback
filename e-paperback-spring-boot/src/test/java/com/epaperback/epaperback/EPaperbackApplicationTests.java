b9f7c44bc099a2c0672911caa7625fcd35de65d9
Kwan <kwanxcsv@gamil.com>
2023-04-24 14:57:08 +0800

    Upload files

---
+++ e-paperback-spring-boot/src/test/java/com/epaperback/epaperback/EPaperbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epaperback.epaperback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPaperbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
