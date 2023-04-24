c14fad0e945f48dc00a4aae954325ab4b64b43c3
Kwan <kwanxcsv@gamil.com>
2023-04-24 14:59:42 +0800

    upload files

---
+++ e-paperback-backend/src/main/java/com/epaperback/epaperback/EPaperbackApplication.java
@@ -0,0 +1,13 @@
+package com.epaperback.epaperback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPaperbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPaperbackApplication.class, args);
+	}
+
+}
